e7c5e47031da9589d012cd0242ddfb21ab0c5304
Vishnu Nair <vish@vnair.me>
2019-01-10 15:41:49 -0500

    A tweak.

--- main.py
+++ main.py
@@ -68,158 +68,160 @@ def post_notification(title, body):
 
 # some needed variables
 CHARS_PER_LINE = 75
-RESULT_IS_DICT_LOOKUP = False
 GTRANS_REAL_URL = "https://translate.google.com/#view=home&op=translate&sl={}&tl={}&text={}"
 GLOSBE_API_URL = "https://glosbe.com/gapi/translate?from={}&dest={}&phrase={}&format=json&pretty=false"
 GLOSBE_REAL_URL = "https://glosbe.com/{}/{}/{}"
-
 CONFIG_FILE_PATH = os.path.expanduser("~/.quick-translator.ini")
-CONFIG_REWRITE_NEEDED = False
 
-# get config
-config = configparser.ConfigParser()
-config.read(CONFIG_FILE_PATH)
-try:
-    config['TRANSLATION_OPTIONS']
-except KeyError:
-    config['TRANSLATION_OPTIONS'] = {}
-    CONFIG_REWRITE_NEEDED = True
-opts = config['TRANSLATION_OPTIONS']
+if __name__ == "__main__":
 
-if 'source_lang' not in opts:
-    config['TRANSLATION_OPTIONS']['source_lang'] = "auto"
-    CONFIG_REWRITE_NEEDED = True
-if 'destination_lang' not in opts:
-    config['TRANSLATION_OPTIONS']['destination_lang'] = "en"
-    CONFIG_REWRITE_NEEDED = True
+    RESULT_IS_DICT_LOOKUP = False
+    CONFIG_REWRITE_NEEDED = False
 
-# define source and destination languages using the config file
-SRC_LANG = opts.get('source_lang', 'auto')
-DEST_LANG = opts.get('destination_lang', 'en')
-
-# save the config file if needed
-if CONFIG_REWRITE_NEEDED:
-    with open(CONFIG_FILE_PATH, 'w+') as configfile:
-        configfile.truncate(0)
-        configfile.seek(0)
-        config.write(configfile)
-
-# get the last selected text via xsel
-selected_text = get_currently_selected_or_copied_text()
-
-# initialize the translator and try to determine the source language for a dictionary lookup
-translator = Translator()
-if SRC_LANG == "auto":
-    SRC_LANG = translator.detect(selected_text).lang[:2]
-
-# convert the new source and defined destination languages into ISO-639-3 form
-try:
-    ISO6393_src_lang = pycountry.languages.get(alpha_2=SRC_LANG).alpha_3
-    ISO6393_dest_lang = pycountry.languages.get(alpha_2=DEST_LANG).alpha_3
-except AttributeError:  # TODO: notify-send currently Linux only, expand.
-    post_notification("Invalid source or destination language in config file?",
-                      "The defined source or destination language is invalid! [S: {}; D: {}, Config file: {}]"
-                      .format(SRC_LANG, DEST_LANG, CONFIG_FILE_PATH))
-    sys.exit(0)
-
-# build the needed urls
-this_glosbe_api_url = GLOSBE_API_URL.format(ISO6393_src_lang, ISO6393_dest_lang, selected_text)
-this_glosbe_url = GLOSBE_REAL_URL.format(SRC_LANG, DEST_LANG, selected_text)
-this_gt_url = GTRANS_REAL_URL.format(SRC_LANG, DEST_LANG, selected_text)
-
-# determine if a dictionary result is in order
-# only do this for source text that is sufficiently short
-if len(selected_text.split(" ")) < 5:
-    rd = requests.get(this_glosbe_api_url).json()
+    # get config
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE_PATH)
     try:
-        if len(rd["tuc"]) != 0: # if we do have results...
-            RESULT_IS_DICT_LOOKUP = True
+        config['TRANSLATION_OPTIONS']
     except KeyError:
-        pass  # keep RESULT_IS_DICT_LOOKUP as False
+        config['TRANSLATION_OPTIONS'] = {}
+        CONFIG_REWRITE_NEEDED = True
+    opts = config['TRANSLATION_OPTIONS']
 
-# even if source lang == dest lang, Glosbe can still return a dictionary result, but a full translation makes no sense in this case
-# so if source lang == dest lang, but Glosbe did not return any results for this term, terminate here
-if SRC_LANG == DEST_LANG and not RESULT_IS_DICT_LOOKUP:
-    post_notification("No dictionary lookup results and nothing to translate!",
-                      "The source and destination languages are the same. [{}]".format(SRC_LANG))
-    sys.exit(0)
+    if 'source_lang' not in opts:
+        config['TRANSLATION_OPTIONS']['source_lang'] = "auto"
+        CONFIG_REWRITE_NEEDED = True
+    if 'destination_lang' not in opts:
+        config['TRANSLATION_OPTIONS']['destination_lang'] = "en"
+        CONFIG_REWRITE_NEEDED = True
 
-# determine the resulting window height and (if needed) full translation
-if RESULT_IS_DICT_LOOKUP:
-    # rd = requests.get(this_glosbe_api_url).json()
-    W_HEIGHT = 30
-else:
-    translated_text = translator.translate(selected_text, src=SRC_LANG, dest=DEST_LANG).text
-    W_HEIGHT = ceil(len(selected_text) / CHARS_PER_LINE) + ceil(len(translated_text) / CHARS_PER_LINE) + 12
+    # define source and destination languages using the config file
+    SRC_LANG = opts.get('source_lang', 'auto')
+    DEST_LANG = opts.get('destination_lang', 'en')
 
-# build the window
-root = Tk()
-root.title('Translation Result' if not RESULT_IS_DICT_LOOKUP else "Lookup Result")
-root.bind('<Escape>', close)
+    # save the config file if needed
+    if CONFIG_REWRITE_NEEDED:
+        with open(CONFIG_FILE_PATH, 'w+') as configfile:
+            configfile.truncate(0)
+            configfile.seek(0)
+            config.write(configfile)
 
-S = Scrollbar(root)
-S.pack(side=RIGHT, fill=Y)
+    # get the last selected text via xsel
+    selected_text = get_currently_selected_or_copied_text()
 
-T = Text(root, height=W_HEIGHT, width=CHARS_PER_LINE, wrap=WORD)
+    # initialize the translator and try to determine the source language for a dictionary lookup
+    translator = Translator()
+    if SRC_LANG == "auto":
+        SRC_LANG = translator.detect(selected_text).lang[:2]
 
-# configure some common tags and bindings
-T.tag_configure('main_header', font=('Times New Roman', 14, 'bold'))
-T.tag_configure('footer', font=('Times New Roman', 9))
-T.tag_configure('footer_link', foreground="blue", font=('Times New Roman', 9, 'underline'))
-T.tag_bind('footer_link', '<Button-1>', open_link)
-T.tag_configure('header_cont', font=('Times New Roman', 12))
+    # convert the new source and defined destination languages into ISO-639-3 form
+    try:
+        ISO6393_src_lang = pycountry.languages.get(alpha_2=SRC_LANG).alpha_3
+        ISO6393_dest_lang = pycountry.languages.get(alpha_2=DEST_LANG).alpha_3
+    except AttributeError:
+        post_notification("Invalid source or destination language in config file?",
+                          "The defined source or destination language is invalid! [S: {}; D: {}, Config file: {}]"
+                          .format(SRC_LANG, DEST_LANG, CONFIG_FILE_PATH))
+        sys.exit(0)
 
-if not RESULT_IS_DICT_LOOKUP:   # if this is a full translation result...
-    T.tag_configure('sect_header', font=('Times New Roman', 12, 'italic'))
-    T.tag_configure('text', font=('Times New Roman', 12, 'bold'))
+    # build the needed urls
+    this_glosbe_api_url = GLOSBE_API_URL.format(ISO6393_src_lang, ISO6393_dest_lang, selected_text)
+    this_glosbe_url = GLOSBE_REAL_URL.format(SRC_LANG, DEST_LANG, selected_text)
+    this_gt_url = GTRANS_REAL_URL.format(SRC_LANG, DEST_LANG, selected_text)
 
-    T.insert(END, "Text translation results ({} to {})\n".format(SRC_LANG.upper(), DEST_LANG.upper()), 'main_header')
-    T.insert(END, "Translation by Google Translate. ", 'footer')
-    T.insert(END, "View online.", ('footer_link', this_gt_url))
-    T.insert(END, "\n\n")
-
-    T.insert(END, "Source ({}):\n\n".format(SRC_LANG), 'sect_header')
-    T.insert(END, "{}".format(selected_text), 'text')
-    T.insert(END, "\n\n{}\n\n".format('-'*CHARS_PER_LINE))
-    T.insert(END, "Translation ({}):\n\n".format(DEST_LANG), 'sect_header')
-    T.insert(END, "{}".format(translated_text), 'text')
-
-else:   # if this is a dictionary lookup result...
-    T.tag_configure('phrase_main', font=('Times New Roman', 12, 'italic'))
-    T.tag_configure('phrase_meaning', font=('Times New Roman', 11))
-    T.tag_configure('phrase_meaning_source', font=('Times New Roman', 8))
-    T.tag_configure('phrase_meaning_source_link', foreground="blue", font=('Times New Roman', 8, 'underline'))
-
-    T.tag_bind('phrase_meaning_source_link', '<Button-1>', open_link)
-
-    defs = rd["tuc"]
-    sources = rd["authors"]
-
-    T.insert(END, "{}\n".format(selected_text), 'main_header')
-    T.insert(END, "Dictionary lookup results ({} to {})\n".format(SRC_LANG.upper(), DEST_LANG.upper()), 'header_cont')
-    T.insert(END, "Lookup results from Glosbe. ", 'footer')
-    T.insert(END, "View detailed results online.", ('footer_link', this_glosbe_url))
-    T.insert(END, "\n\n")
-
-    for d in defs:
+    # determine if a dictionary result is in order
+    # only do this for source text that is sufficiently short
+    if len(selected_text.split(" ")) < 5:
+        rd = requests.get(this_glosbe_api_url).json()
         try:
-            phrase = html.unescape(d["phrase"]["text"]) # some Glosbe results have HTML entities so unescape them
+            if len(rd["tuc"]) != 0: # if we do have results...
+                RESULT_IS_DICT_LOOKUP = True
         except KeyError:
-            phrase = "---"
+            pass  # keep RESULT_IS_DICT_LOOKUP as False
 
-        T.insert(END, "{}\n".format(phrase), 'phrase_main')
-        try:
-            all_meanings = d["meanings"]
-        except KeyError:
-            all_meanings = []
+    # even if source lang == dest lang, Glosbe can still return a dictionary result, but a full translation makes no sense in this case
+    # so if source lang == dest lang, but Glosbe did not return any results for this term, terminate here
+    if SRC_LANG == DEST_LANG and not RESULT_IS_DICT_LOOKUP:
+        post_notification("No dictionary lookup results and nothing to translate!",
+                          "The source and destination languages are the same. [{}]".format(SRC_LANG))
+        sys.exit(0)
 
-        for m in all_meanings:
-            T.insert(END, "\t* {} [{}]\n".format(clean_text(html.unescape(m["text"])), m["language"]), 'phrase_meaning')
+    # determine the resulting window height and (if needed) full translation
+    if RESULT_IS_DICT_LOOKUP:
+        # rd = requests.get(this_glosbe_api_url).json()
+        W_HEIGHT = 30
+    else:
+        translated_text = translator.translate(selected_text, src=SRC_LANG, dest=DEST_LANG).text
+        W_HEIGHT = ceil(len(selected_text) / CHARS_PER_LINE) + ceil(len(translated_text) / CHARS_PER_LINE) + 12
 
-        T.insert(END, "\tSource(s): ", 'phrase_meaning_source')
-        for a in d["authors"]:
-            T.insert(END, "{} ".format(sources[str(a)]["N"]), ('phrase_meaning_source_link', sources[str(a)]["url"]))
+    # build the window
+    root = Tk()
+    root.title('Translation Result' if not RESULT_IS_DICT_LOOKUP else "Lookup Result")
+    root.bind('<Escape>', close)
+
+    S = Scrollbar(root)
+    S.pack(side=RIGHT, fill=Y)
+
+    T = Text(root, height=W_HEIGHT, width=CHARS_PER_LINE, wrap=WORD)
+
+    # configure some common tags and bindings
+    T.tag_configure('main_header', font=('Times New Roman', 14, 'bold'))
+    T.tag_configure('footer', font=('Times New Roman', 9))
+    T.tag_configure('footer_link', foreground="blue", font=('Times New Roman', 9, 'underline'))
+    T.tag_bind('footer_link', '<Button-1>', open_link)
+    T.tag_configure('header_cont', font=('Times New Roman', 12))
+
+    if not RESULT_IS_DICT_LOOKUP:   # if this is a full translation result...
+        T.tag_configure('sect_header', font=('Times New Roman', 12, 'italic'))
+        T.tag_configure('text', font=('Times New Roman', 12, 'bold'))
+
+        T.insert(END, "Text translation results ({} to {})\n".format(SRC_LANG.upper(), DEST_LANG.upper()), 'main_header')
+        T.insert(END, "Translation by Google Translate. ", 'footer')
+        T.insert(END, "View online.", ('footer_link', this_gt_url))
         T.insert(END, "\n\n")
 
-T.pack(side=LEFT, fill=Y)
-mainloop()
+        T.insert(END, "Source ({}):\n\n".format(SRC_LANG), 'sect_header')
+        T.insert(END, "{}".format(selected_text), 'text')
+        T.insert(END, "\n\n{}\n\n".format('-'*CHARS_PER_LINE))
+        T.insert(END, "Translation ({}):\n\n".format(DEST_LANG), 'sect_header')
+        T.insert(END, "{}".format(translated_text), 'text')
+
+    else:   # if this is a dictionary lookup result...
+        T.tag_configure('phrase_main', font=('Times New Roman', 12, 'italic'))
+        T.tag_configure('phrase_meaning', font=('Times New Roman', 11))
+        T.tag_configure('phrase_meaning_source', font=('Times New Roman', 8))
+        T.tag_configure('phrase_meaning_source_link', foreground="blue", font=('Times New Roman', 8, 'underline'))
+
+        T.tag_bind('phrase_meaning_source_link', '<Button-1>', open_link)
+
+        defs = rd["tuc"]
+        sources = rd["authors"]
+
+        T.insert(END, "{}\n".format(selected_text), 'main_header')
+        T.insert(END, "Dictionary lookup results ({} to {})\n".format(SRC_LANG.upper(), DEST_LANG.upper()), 'header_cont')
+        T.insert(END, "Lookup results from Glosbe. ", 'footer')
+        T.insert(END, "View detailed results online.", ('footer_link', this_glosbe_url))
+        T.insert(END, "\n\n")
+
+        for d in defs:
+            try:
+                phrase = html.unescape(d["phrase"]["text"]) # some Glosbe results have HTML entities so unescape them
+            except KeyError:
+                phrase = "---"
+
+            T.insert(END, "{}\n".format(phrase), 'phrase_main')
+            try:
+                all_meanings = d["meanings"]
+            except KeyError:
+                all_meanings = []
+
+            for m in all_meanings:
+                T.insert(END, "\t* {} [{}]\n".format(clean_text(html.unescape(m["text"])), m["language"]), 'phrase_meaning')
+
+            T.insert(END, "\tSource(s): ", 'phrase_meaning_source')
+            for a in d["authors"]:
+                T.insert(END, "{} ".format(sources[str(a)]["N"]), ('phrase_meaning_source_link', sources[str(a)]["url"]))
+            T.insert(END, "\n\n")
+
+    T.pack(side=LEFT, fill=Y)
+    mainloop()
